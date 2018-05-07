f4164ef99d0955a0d503f1571463105026a37cd3
Stefan Schallenberg <nafets227@users.noreply.github.com>
2018-05-07 21:52:41 +0200

    Added timers to physerver

---
+++ install/timer.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+#
+# (C) 2015-2018 Stefan Schallenberg
+# (C) 2016 Veronika Ruoff
+#
+
+##### install-timer ##########################################################
+function install-timer {
+	name="$1"
+	script="$2"
+	onBootSec="$3"
+	onUnitActiveSec="$4"
+	onCalendar="$5"
+	user="${6:-root}"
+
+        #----- Input checks --------------------------------------------------
+        if [ ! -d "$INSTALL_ROOT" ] ; then
+                printf "%s: Error \$INSTALL_ROOT=%s is no directory\n" \
+                        "$FUNCNAME" "$INSTALL_ROOT" >&2
+                return 1
+	elif [ "$#" -lt 1 ] ; then
+		printf "%s: Internal Error. Got %s parms (Exp=1+)\n" \
+			"$FUNCNAME" "$#"
+		return 1
+	elif [ -z "$onBootSec" ] && \
+	     [ -z "$onUnitActiveSec" ] && \
+	     [ -z "$onCalendar" ] ; then
+		printf "%s: Internal Error. Neither parm 3,4 or 5 are set.\n" \
+			"$FUNCNAME"
+		return 1
+        fi
+
+        #----- Real Work -----------------------------------------------------
+	FNTIMER="$INSTALL_ROOT/etc/systemd/system/${name}.timer"
+	cat >$FNTIMER <<-EOF
+		# nafetsde-${name}.timer
+		#
+		# (C) 2016-2018 Stefan Schallenberg
+		#
+		[Unit]
+		Description="$name Automatismen auf nafets.de (timer)"
+
+		[Install]
+		WantedBy=multi-user.target
+
+		[Timer]
+		Unit=${name}.service
+		EOF
+	[ ! -z "$onBootSec" ] &&
+		printf "OnBootSec=%s\n" "$onBootSec" >>$FNTIMER
+	[ ! -z "$onUnitActiveSec" ] &&
+		printf "OnUnitActiveSet=%s\n" "$onUnitActiveSec" >>$FNTIMER
+	[ ! -z "$onCalendar" ] &&
+		printf "OnCalendar=%s\n" "$onCalendar" >>$FNTIMER
+
+	FNSERVICE="$INSTALL_ROOT/etc/systemd/system/${name}.service"
+	cat >$FNSERVICE <<-EOF
+		# nafetsde-${name}.service
+		#
+		# (C) 2015-2018 Stefan Schallenberg
+		#
+		[Unit]
+		Description="$name Automatismen auf nafets.de (service)"
+
+		[Service]
+		Type=simple
+		ExecStart=$script
+		User=$user
+		EOF
+
+        #----- Closing  ------------------------------------------------------
+	printf "Timer %s to call %s as %s [%s, %s, %s] uccessfully setup.\n" \
+		"$name" "$script" "$user" \
+		"$onBootSec" "$onUnitActiveSec" "$onCalendar"
+
+	return 0
+}
+
