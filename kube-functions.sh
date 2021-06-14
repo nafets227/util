300fa92322011723af1b252fdb84ea2da239fa26
Stefan Schallenberg <nafets227@users.noreply.github.com>
2021-06-14 21:53:19 +0200

    refactor kube-functions.sh

--- kube-functions.sh
+++ kube-functions.sh
@@ -15,10 +15,12 @@
 #   5 - Kube Configfile (defaults to ~/.kube/$stage.conf )
 # Environment Variables set on exit
 #   KUBE_CONFIGFILE name of Kubeconfig with access credentials
-#   KUBE_ACTION     action
-#   KUBE_STAGE      stage
-#   KUBE_NAMESPACE  Namespeace to be used
-#   KUBE_APP        Application name to be used in Labels
+#   KUBE_ACTION      action
+#   KUBE_CLI_ACTION  action to be used in kubectl cli
+#   KUBE_ACTION_DISP action to be displayed to the user with -ing
+#   KUBE_STAGE       stage
+#   KUBE_NAMESPACE   Namespeace to be used
+#   KUBE_APP         Application name to be used in Labels
 function kube-inst_init {
 	local action="$1"
 	local stage="${2:-preprod}"
@@ -26,10 +28,18 @@ function kube-inst_init {
 	local ns="$4"
 	local configfile="$5"
 
-	if		[ "$action" == "install" ] ||
-			[ "$action" == "delete" ] ||
-			[ "$action" == "config" ] ; then
+	if [ "$action" == "install" ] ; then
 		KUBE_ACTION="$action"
+		KUBE_CLI_ACTION="apply"
+		KUBE_ACTION_DISP="Installing"
+	elif [ "$action" == "delete" ] ; then
+		KUBE_ACTION="$action"
+		KUBE_CLI_ACTION="delete"
+		KUBE_ACTION_DISP="Deleting"
+	elif [ "$action" == "config" ] ; then
+		KUBE_ACTION="$action"
+		KUBE_CLI_ACTION="error" # produce error if anything should be executed
+		KUBE_ACTION_DISP="Configuring"
 	else
 		printf "Invalid Action %s\n" "$action"
 		return 1
@@ -65,12 +75,18 @@ function kube-inst_init {
 function kube-inst_internal-verify-initialised {
 	if 	 [ ! -r "$KUBE_CONFIGFILE" ] ||
 			[ -z "$KUBE_ACTION" ] ||
+			[ -z "$KUBE_CLI_ACTION" ] ||
+			[ -z "$KUBE_ACTION_DISP" ] ||
 			[ -z "$KUBE_NAMESPACE" ] ||
 			[ -z "$KUBE_STAGE" ] ||
 			[ -z "$KUBE_APP" ] ; then
 		printf "kube-inst_init has not been called. \n"
-		printf "KUBE_CONFIGFILE, KUBE_ACTION, KUBE_NAMESPACE, "
+		printf "KUBE_CONFIGFILE, KUBE_ACTION, KUBE_CLI_ACTION, "
+		printf "KUBE_ACTION_DISP, KUBE_NAMESPACE, "
 		printf "KUBE_STAGE or KUBE_APP is not set.\n"
+		return 1
+	elif [ "$KUBE_ACTION" == "config" ] ; then
+		printf "cannot execute with KUBE_ACTION=config\n"
 		return 1
 	else
 		return 0
@@ -110,28 +126,115 @@ function kube-inst_internal-environize {
 	return 0
 }
 
+##### kube-inst_internal-exec ################################################
+# Execute a Template File
+# Parameters:
+#   1 - Name of the templace file. Can be "-" for stdin
+#   2 - Name of Environment variables to substitute. Canb be empty in which
+#       case no substitution takes place.
+#   3ff - Kubectl options
+function kube-inst_internal-exec {
+	local file="$1"
+	shift
+	local envnames="$1"
+	shift
+
+	cat $file |
+		kube-inst_internal-environize "$envnames" |
+		kubectl \
+			--kubeconfig $KUBE_CONFIGFILE \
+			$KUBE_CLI_ACTION \
+			-n $KUBE_NAMESPACE \
+			"$@" \
+			-f -
+
+	return $?
+}
+
 ##### kube-inst_exec - Execute installation ##################################
 # Prerequisite: kube-inst_init has been called
-# Parametets:
-#   1 - confdir
-#   2 - envnames
+# Parameters:
+#   1 - confdir Directory containing all yamls and templates
+#       [default=./kube]
+#   2 - envnames [ default=none ]
+#       you can  give alist of names to be environized. All words in .template files
+#       inside the configdir that match a name in the list will be replaced by teh
+#       value of the environment variable with that name.
+#       Example:
+#       envnames="MYNAME MYIP"
+#       Environment variable MYNAME="mynamevalue"
+#       Envrionment variabddle MYIP="myipvalue"
+#       my.yaml.template:
+#           value=${MYNAME}
+#          ip=${MYIP}
+#       will be transformed to
+#  	        value=mynamevalue
+#           ip=myipvalue
+#       before the yaml file will be processed by kubernetes.
 function kube-inst_exec {
 	local confdir="$(realpath ${1:-./kube})"
-	local envnames="$2"
+	local envnames="$2 KUBE_APP"
 
 	kube-inst_internal-verify-initialised || return 1
 
-	KUBECONFIG=$KUBE_CONFIGFILE kube-inst_internal \
-		"$KUBE_ACTION" \
-		"$KUBE_APP" \
-		"$KUBE_NAMESPACE" \
-		"$confdir" \
-		"$envnames KUBE_APP"
-	rc="$?"
+	##### checking parameters
+	if [ "$#" -lt 2 ] ; then
+		printf "%s: Error. received %s parms (Exp >=2 ).\n" \
+			"$FUNCNAME" "$#"
+		return 1
+	fi
+
+	if [ ! -d $confdir ] ; then
+		printf "%s: Error confdir \"%s\" is no directory.\n"
+		return 1
+	fi
+
+	##### Action !
+	printf "%s kube-app \"%s\" in namespace \"%s\"" \
+		"$KUBE_ACTION_DISP" "$KUBE_APP" "$KUBE_NAMESPACE"
+	printf " (Env: %s) from \"%s\".\n" \
+		"$envnames" "$confdir"
+	for f in $envnames ; do
+		eval "value=\$$f"
+		printf "\t %s=%s\n" "$f" "$value"
+	done
+
+	#Execute Shell Scripts in confdir
+	if [ ! -z "$(ls $confdir/*.sh 2>/dev/null)" ] ; then
+		for f in $confdir/*.sh ; do
+			printf "Loading Kubeconfig %s ... " "$(basename $f)"
+			. $f --$KUBE_ACTION || return 1
+		done
+	fi
+
+	#Execute yaml in confdir
+	if [ ! -z "$(ls $confdir/*.yaml 2>/dev/null)" ] ; then
+		for f in $confdir/*.yaml ; do
+			printf "Loading Kubeconfig %s ... " "$(basename $f)"
+			kube-inst_internal-exec $f || return 1
+		done
+	fi
+
+	#Execute yaml.template in confdir
+	if [ ! -z "$(ls $confdir/*.yaml.template 2>/dev/null)" ] ; then
+		for f in $confdir/*.yaml.template ; do
+			printf "Loading Kubeconfig %s ... " "$(basename $f)"
+			kube-inst_internal-exec $f 	"$envnames" || return 1
+		done
+	fi
+
+	#Execute yaml.delete in confdir
+	if [ ! -z $(ls $confdir/*.yaml.delete 2>/dev/null) ] ; then
+		for f in $confdir/*.yaml.delete ; do
+			kubectl delete -n $ns -f $f \
+				--cascade=true --ignore-not-found \
+			|| return 1
+		done
+	fi
 
 	unset KUBE_CONFIGFILE KUBE_ACTION KUBE_NAMESPACE KUBE_APP
 
-	return $rc
+	return 0
 }
 
 ##### kube-inst_helm2 - install Helm Chart from repo #########################
@@ -284,18 +387,6 @@ function kube-inst_tls-secret {
 		return 1
 	fi
 
-	if [ "$KUBE_ACTION" == "install" ] ; then
-		kube_action="apply"
-		action_display="Installing"
-	elif [ "$KUBE_ACTION" == "delete" ] ; then
-		kube_action="delete"
-		action_display="Deleting"
-	else
-		printf "%s: Error. Action (Parm1) %s unknown." \
-			"$FUNCNAME" "$1"
-		printf " Must be \"install\" or \"delete\".\n"
-		return 1
-	fi
 
 	# @TODO handle delete correctly, do not create a new cert !
 	printf "creating secret %s ... " "$secretname"
@@ -311,7 +402,7 @@ function kube-inst_tls-secret {
 		--save-config \
 		--dry-run \
 		-o yaml \
-	| kubectl --kubeconfig $KUBE_CONFIGFILE $kube_action -n $KUBE_NAMESPACE -f - \
+	| kubectl --kubeconfig $KUBE_CONFIGFILE $KUBE_CLI_ACTION -n $KUBE_NAMESPACE -f - \
 	|| return 1
 
 	return 0
@@ -340,19 +431,7 @@ function kube-inst_generic-secret {
 	# it may be also a directory or a string like logicalname=realname
 	fi
 
-	if [ "$KUBE_ACTION" == "install" ] ; then
-		kube_action="apply"
-		action_display="Installing"
-	elif [ "$KUBE_ACTION" == "delete" ] ; then
-		kube_action="delete"
-		action_display="Deleting"
-	else
-		printf "%s: Error. Action \$KUBE_ACTION=%s unknown." \
-			"$FUNCNAME" "$1"
-		printf " Must be \"install\" or \"delete\".\n"
-		return 1
-	fi
-	printf "creating secret %s ... " "$secretname"
+	printf "%s secret %s ... " "$KUBE_ACTION_DISP" "$secretname"
 
 	local fromfilearg=""
 	for a in "$@" ; do
@@ -365,7 +444,7 @@ function kube-inst_generic-secret {
 		--save-config \
 		--dry-run \
 		-o yaml \
-	| kubectl --kubeconfig $KUBE_CONFIGFILE $kube_action -n $KUBE_NAMESPACE -f - \
+	| kubectl --kubeconfig $KUBE_CONFIGFILE $KUBE_CLI_ACTION -n $KUBE_NAMESPACE -f - \
 	|| return 1
 
 	return 0
@@ -394,19 +473,7 @@ function kube-inst_configmap {
 	# it may be also a directory or a string like logicalname=realname
 	fi
 
-	if [ "$KUBE_ACTION" == "install" ] ; then
-		kube_action="apply"
-		action_display="Installing"
-	elif [ "$KUBE_ACTION" == "delete" ] ; then
-		kube_action="delete"
-		action_display="Deleting"
-	else
-		printf "%s: Error. Action \$KUBE_ACTION=%s unknown." \
-			"$FUNCNAME" "$1"
-		printf " Must be \"install\" or \"delete\".\n"
-		return 1
-	fi
-	printf "creating configmap %s ... " "$secretname"
+	printf "%s configmap %s ... " "$KUBE_ACTION_DISP" "$secretname"
 
 	local fromfilearg=""
 	for a in "$@" ; do
@@ -419,7 +486,7 @@ function kube-inst_configmap {
 		--save-config \
 		--dry-run \
 		-o yaml \
-	| kubectl --kubeconfig $KUBE_CONFIGFILE $kube_action -n $KUBE_NAMESPACE -f - \
+	| kubectl --kubeconfig $KUBE_CONFIGFILE $KUBE_CLI_ACTION -n $KUBE_NAMESPACE -f - \
 	|| return 1
 
 	return 0
@@ -439,6 +506,7 @@ function kube-inst_nfs-volume {
 
 	local readonly nfsserver="${path%%:*}"
 	local readonly nfspath="${path##*:}"
+	local opt=""
 
 	kube-inst_internal-verify-initialised || return 1
 
@@ -448,18 +516,8 @@ function kube-inst_nfs-volume {
 		return 1
 	fi
 
-	if [ "$KUBE_ACTION" == "install" ] ; then
-		kube_action="apply"
-		action_display="Installed"
-	elif [ "$KUBE_ACTION" == "delete" ] ; then
-		kube_action="delete --wait=false"
-		action_display="Deleted"
-	else
-		printf "%s: Error. Action (Parm1) %s unknown." \
-			"$FUNCNAME" "$1"
-		printf " Must be \"install\" or \"delete\".\n"
-		return 1
-	fi
+	printf "%s NFS-Volume %s (app=%s, stage=%s) for %s\n" \
+		"$KUBE_ACTION_DISP" "$share" "$KUBE_APP" "$KUBE_STAGE" "$path"
 
 	#### Make sure directory exists on server
 	if [ "$KUBE_ACTION" == "install" ] ; then
@@ -467,26 +525,23 @@ function kube-inst_nfs-volume {
 			"test -d /srv/nfs4/$nfspath" \
 			'||' "mkdir -p /srv/nfs4/$nfspath" \
 			|| return 1
+
+		if [ ! -z "$owner" ] ; then
+			ssh -o StrictHostKeyChecking=no $nfsserver \
+				"chown -R $owner /srv/nfs4/$nfspath" \
+				|| return 1
+		fi
+	elif [ "$KUBE_ACTION" == "delete" ] ; then
+		# when deleting we leave the data untouched !
+		opt="--wait=0"
 	fi
-	# when deleting we leave the data untouched !
 
-	if [ ! -z "$owner" ] ; then
-		ssh -o StrictHostKeyChecking=no $nfsserver \
-			"chown -R $owner /srv/nfs4/$nfspath" \
-			|| return 1
-	fi
+	kube-inst_internal-exec \
+		$(dirname "$BASH_SOURCE")/kube/nfsVolume.yaml.template \
+		"KUBE_APP KUBE_STAGE share nfspath nfsserver KUBE_NAMESPACE" \
+		$opt
 
-	#DEBUG# printf "Adding PV %s as %s:%s\n" "$share.$app.$stage" "$nfsserver" "$path"
-	#### Setup Persistent Volume
-	kube-inst_internal-environize "KUBE_APP KUBE_STAGE share nfspath nfsserver KUBE_NAMESPACE" \
-		<$(dirname "$BASH_SOURCE")/kube/nfsVolume.yaml.template \
-	| kubectl --kubeconfig $KUBE_CONFIGFILE $kube_action -f - &&
-		true || return 1
-
-		printf "%s Volume %s (app=%s, stage=%s) for %s\n" \
-			"$action_display" "$share" "$KUBE_APP" "$KUBE_STAGE" "$path"
-
-		return 0
+	return $?
 }
 
 ##### kube-inst_host-volume - Install Host Volume ############################
@@ -497,6 +552,7 @@ function kube-inst_nfs-volume {
 function kube-inst_host-volume {
 	local share="$1"
 	local path="$2"
+	local opt=""
 
 	kube-inst_internal-verify-initialised || return 1
 
@@ -506,146 +562,23 @@ function kube-inst_host-volume {
 		return 1
 	fi
 
-	if [ "$KUBE_ACTION" == "install" ] ; then
-		kube_action="apply"
-		action_display="Installed"
-	elif [ "$KUBE_ACTION" == "delete" ] ; then
-		kube_action="delete --wait=false"
-		action_display="Deleted"
-	else
-		printf "%s: Error. Action (Parm1) %s unknown." \
-		   "$FUNCNAME" "$1"
-		printf " Must be \"install\" or \"delete\".\n"
-		return 1
-	fi
+	printf "%s Host-Volume %s (app=%s, stage=%s) for %s\n" \
+		"$KUBE_ACTION_DISP" "$share" "$KUBE_APP" "$KUBE_STAGE" "$path"
 
 	#### Make sure directory exists
 	if [ "$KUBE_ACTION" == "install" ] ; then
 		[ -d "$path" ] || mkdir -p $path || return 1
+	elif [ "$KUBE_ACTION" == "delete" ] ; then
+		opt="--wait=0"
 	fi
 	# when deleting we leave the data untouched !
 
-	#DEBUG# printf "Adding PV %s as %s:%s\n" "$share.$app.$stage" "$nfsserver" "$path"
-	#### Setup Persistent Volume
-	kube-inst_internal-environize "KUBE_APP KUBE_STAGE share path KUBE_NAMESPACE" \
-		<$(dirname "$BASH_SOURCE")/kube/hostVolume.yaml.template \
-	| kubectl --kubeconfig $KUBE_CONFIGFILE $kube_action -f - &&
-		true || return 1
+	kube-inst_internal-exec \
+		$(dirname "$BASH_SOURCE")/kube/hostVolume.yaml.template \
+		"KUBE_APP KUBE_STAGE share path KUBE_NAMESPACE" \
+		$opt
 
-		printf "%s Volume %s (app=%s, stage=%s) for %s\n" \
-			"$action_display" "$share" "$KUBE_APP" "$KUBE_STAGE" "$path"
-
-		return 0
-}
-
-##### kube-inst_internal - install Kubernetes objects in kube/ subdir ##############
-# DEPRECATED
-#   This function is Deprecated, please use kube-inst_init and kube-inst_exec
-#   instead
-# Parameter:
-#   1 - action [ install | delete ]
-#   2 - app Application name to be assigned to kubernetes tag
-#   3 - ns  Namespace for kubernetes objects [default:test]
-#   4 - confdir Directory containing all yamls and templates
-#       [default=./kube]
-#   5 - envnames [ default=none ]
-#       you can  give alist of names to be environized. All words in .template files
-#       inside the configdir that match a name in the list will be replaced by teh
-#       value of the environment variable with that name.
-#       Example:
-#       envnames="MYNAME MYIP"
-#       Environment variable MYNAME="mynamevalue"
-#       Envrionment variabddle MYIP="myipvalue"
-#       my.yaml.template:
-#		value=${MYNAME}
-#               ip=${MYIP}
-#	will be transformed to
-#  		value=mynamevalue
-#		ip=myipvalue
-#	before the yaml file will be processed by kubernetes.
-function kube-inst_internal {
-	local action="$1"
-	local app="$2"
-	local ns="$3"
-	local confdir="$(realpath ${4:-./kube})"
-	local envnames="$5"
-	local kube_action action_display
-
-	##### checking parameters
-	if [ "$#" -lt 2 ] ; then
-		printf "%s: Error. received %s parms (Exp >=2 ).\n" \
-			"$FUNCNAME" "$#"
-		return 1
-	fi
-
-	if [ "$action" == "install" ] ; then
-		kube_action="apply"
-		action_display="Installing"
-	elif [ "$action" == "delete" ] ; then
-		kube_action="delete"
-		action_display="Deleting"
-	else
-		printf "%s: Error. Action (Parm1) %s unknown." \
-			"$FUNCNAME" "$1"
-		printf " Must be \"install\" or \"delete\".\n"
-		return 1
-	fi
-
-	if [ ! -d $confdir ] ; then
-		printf "%s: Error confdir \"%s\" is no directory.\n"
-		return 1
-	fi
-
-	##### Action !
-	printf "%s kube-app \"%s\" in namespace \"%s\"" \
-		"$action_display" "$app" "$ns"
-	printf " (Env: %s) from \"%s\".\n" \
-		"$envnames" "$confdir"
-	for f in $envnames ; do
-		eval "value=\$$f"
-		printf "\t %s=%s\n" "$f" "$value"
-	done
-
-	#Execute Shell Scripts in confdir
-	if [ ! -z "$(ls $confdir/*.sh 2>/dev/null)" ] ; then
-		for f in $confdir/*.sh ; do
-			printf "Loading Kubeconfig %s ... " "$(basename $f)"
-			. $f --$action || return 1
-		done
-	fi
-
-	#Execute yaml in confdir
-	if [ ! -z "$(ls $confdir/*.yaml 2>/dev/null)" ] ; then
-		for f in $confdir/*.yaml ; do
-			printf "Loading Kubeconfig %s ... " "$(basename $f)"
-			kubectl $kube_action -n $ns -f $f || \
-				kubectl replace -n $ns -f $f \
-			|| return 1
-		done
-	fi
-
-	#Execute yaml.template in confdir
-	if [ ! -z "$(ls $confdir/*.yaml.template 2>/dev/null)" ] ; then
-		for f in $confdir/*.yaml.template ; do
-			printf "Loading Kubeconfig %s ... " "$(basename $f)"
-			#debug printf "YAML with templace parms replaced: \n"
-			#debug kube-inst_internal-environize <$f
-			kube-inst_internal-environize "$envnames" <$f \
-			| kubectl $kube_action -n $ns -f - \
-			|| return 1
-		done
-	fi
-
-	#Execute yaml.delete in confdir
-	if [ ! -z $(ls $confdir/*.yaml.delete 2>/dev/null) ] ; then
-		for f in $confdir/*.yaml.delete ; do
-			kubectl delete -n $ns -f $f \
-				--cascade=true --ignore-not-found \
-			|| return 1
-		done
-	fi
-
-	return 0
+	return $?
 }
 
 ##### MAIN-template ##################################################################
