7b48b5fc2bb7f960788f36ca6cd67dbc0edb6779
Stefan Schallenberg <nafets227@users.noreply.github.com>
2024-04-14 13:07:49 +0200

    inst-arch: wait for udev after wiping FSs

--- inst-arch-functions.sh
+++ inst-arch-functions.sh
@@ -242,6 +242,8 @@ function inst-arch_destroy() {
 		wipefs --all --force "$bootdev" || return 1
 	fi
 
+	udevadm settle # wait udev to proceed wiped FSs
+
 	return 0
 }
 
@@ -258,6 +260,7 @@ function inst-arch_destroy-disk () {
 
 	if [ -b "$diskdev" ] ; then
 		wipefs --all --force "$diskdev" || return 1
+		udevadm settle # wait udev to proceed wiped FSs
 	elif [ -e "$diskdev" ] ; then
 		rm "$diskdev" || return 1
 	fi
